3115836585dc9c8b9f7942d19e3b994e9afcf744
dongboyang00 <dongboyang00@gmail.com>
2023-03-09 14:31:46 -0700

    Adding Card and CardList components to display random images of robots

---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((user, index) => {
+    return (
+      <Card key={index} id={user.id} name={user.name} email={user.email} />
+    );
+  });
+  return <div>{cardComponent}</div>;
+};
+
+export default CardList;
